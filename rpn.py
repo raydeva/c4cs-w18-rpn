901fb4f7acf5ee9c2e64bef159d12389762cc815
Rayka Devaprasad <rayka@umich.edu>
2018-02-23 10:25:56 -0500

    Implement sutract

--- rpn.py
+++ rpn.py
@@ -1,11 +1,32 @@
 #!/usr/bin/env python3
 
 def calculate(arg):
-	pass
+	stack = list()
+	for token in arg.split():
+		try:
+			value = int(token)
+			stack.append(value)
+		except ValueError:
+			if token == '+':
+				arg2 = stack.pop()
+				arg1 = stack.pop()
+				result = arg1 + arg2
+				stack.append(result)
+			elif token == '-':
+				arg2 = stack.pop()
+				arg1 = stack.pop()
+				result = arg1 - arg2
+				stack.append(result)
+			else:
+				print('Not an operator.')
+
+		print(stack)
+
+	return stack.pop()
 
 def main():
 	while True:
-		calculate(input('rpn calc> '))
+		print(calculate(input('rpn calc> ')))
 
 if __name__ == '__main__':
 	main()
