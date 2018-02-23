44caf8f349aa4b8d433efca9e5e6ea20dbf44369
Rayka Devaprasad <rayka@umich.edu>
2018-02-23 10:49:56 -0500

    Added multiplication and division functions

--- rpn.py
+++ rpn.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
 
+import operator
+
+
+operators = {
+	'+': operator.add,
+	'-': operator.sub,
+	'*': operator.mul,
+	'/': operator.truediv,
+}
+
+
 def calculate(arg):
 	stack = list()
 	for token in arg.split():
@@ -7,20 +18,16 @@ def calculate(arg):
 			value = int(token)
 			stack.append(value)
 		except ValueError:
-			if token == '+':
-				arg2 = stack.pop()
-				arg1 = stack.pop()
-				result = arg1 + arg2
-				stack.append(result)
-			elif token == '-':
-				arg2 = stack.pop()
-				arg1 = stack.pop()
-				result = arg1 - arg2
-				stack.append(result)
-			else:
-				print('Not an operator.')
-
+			function = operators[token]
+			arg2 = stack.pop()
+			arg1 = stack.pop()
+			result = function(arg1, arg2)
+			stack.append(result)
+			
 		print(stack)
+
+	if len(stack) != 1:
+		raise TypeError
 
 	return stack.pop()
 
