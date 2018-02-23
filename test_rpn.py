7873080d04abf2ad83590f9f7b601d540018785c
Rayka Devaprasad <rayka@umich.edu>
2018-02-23 10:37:51 -0500

    Add test for too many params... note: not implemented

--- test_rpn.py
+++ test_rpn.py
@@ -11,4 +11,7 @@ class TestBasics(unittest.TestCase):
 	def test_subtract(self):
 		result = rpn.calculate('5 2 -')
 		self.assertEqual(3, result)
+	def test_toomany(self):
+		with self.assertRaises(TypeError):
+			result = rpn.calculate('1 2 3 +')
 		
