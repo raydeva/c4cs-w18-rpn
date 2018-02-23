24d340366c04d7ba5800d6c0b22a050efb2657c3
Rayka Devaprasad <rayka@umich.edu>
2018-02-23 10:17:49 -0500

    Add test for chained addition.

--- test_rpn.py
+++ test_rpn.py
@@ -5,4 +5,7 @@ class TestBasics(unittest.TestCase):
 	def test_add(self):
 		result = rpn.calculate('1 1 +')
 		self.assertEqual(2, result)
+	def test_adds(self):
+		result = rpn.calculate('1 1 + 2 +')
+		self.assertEqual(4, result)
 		
