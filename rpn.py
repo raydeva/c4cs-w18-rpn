46f953a7e900734d855b93204ec28b1234c87eda
raykadevaprasad <rdevaprasad@gmail.com>
2018-03-05 13:32:21 -0500

    added exponentiation operator

--- rpn.py
+++ rpn.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 
 import operator
-
+import math
 
 operators = {
 	'+': operator.add,
 	'-': operator.sub,
 	'*': operator.mul,
 	'/': operator.truediv,
+	'^': operator.pow,
+	'.': operator.floordiv,
+	'!': math.factorial,
+	'&': operator.and_,
+	'|': operator.or_,
+	'~': operator.invert,
 }
 
 
@@ -18,14 +24,30 @@ def calculate(arg):
 			value = int(token)
 			stack.append(value)
 		except ValueError:
-			function = operators[token]
-			arg2 = stack.pop()
-			arg1 = stack.pop()
-			result = function(arg1, arg2)
-			stack.append(result)
-			
+			if(token ==  '!'):
+				function = operators[token]
+				arg1 = stack.pop()
+				result = function(arg1)
+				stack.append(result)
+			elif(token == '%'):
+				value = stack.pop()/100
+				secondValue = stack.pop()
+				stack.append(secondValue)
+				value = value*secondValue
+				stack.append(value)
+			elif(token == '~'):
+				function = operators[token]
+				arg1 = stack.pop()
+				result = function(arg1)
+				stack.append(result)			 
+			else:
+				function = operators[token]
+				arg2 = stack.pop()
+				arg1 = stack.pop()
+				result = function(arg1, arg2)
+				stack.append(result)			
+	
 		print(stack)
-
 	if len(stack) != 1:
 		raise TypeError
 
